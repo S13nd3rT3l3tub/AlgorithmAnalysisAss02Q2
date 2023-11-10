a1bec88c15f3e57ce295749a8e75d38be3677662
S13nd3rT3l3tub <40754433+S13nd3rT3l3tub@users.noreply.github.com>
2023-11-10 10:16:00 +0800

    Theory based coding of reference

--- AlgorithmAnalysisAss02Q2.cpp
+++ AlgorithmAnalysisAss02Q2.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 #include <map>
+#include <set>
+#include <algorithm>
 
 // Based on https://www.geeksforgeeks.org/0-1-knapsack-problem-to-print-all-possible-solutions/
 
@@ -18,7 +19,7 @@ int PermutateThroughKnapSack(std::vector<int> _values,std::vector<int> _weights,
 
 	// Map to store weights with profits of each item
 	//std::map<int, std::pair<int, int>> uMap;
-	std::vector<std::pair<int, int>> itemList{}, possibleList{};
+	std::vector<std::pair<int, int>> itemList{};
 
 	// Create pair containing value and weight of item and insert into container
 	//for (int i{ 0 }; i < _values.size(); ++i)
@@ -27,26 +28,51 @@ int PermutateThroughKnapSack(std::vector<int> _values,std::vector<int> _weights,
 		itemList.emplace_back(std::pair<int, int>{_values[i], _weights[i]});
 
 	// Variables for permutation looping
-	//std::set<std::vector<std::pair<int, int>>> set_sol;
+	std::set<std::vector<std::pair<int, int>>> solutionSet{};
 	int result = INT_MIN;
-	int remaining_weight;
+	int remainingWeight;
 	int sum = 0;
 
 	do {
 		// Set initial values
 		sum = 0;
-		remaining_weight = _maxCapacity;
-		possibleList.clear();
+		remainingWeight = _maxCapacity;
+		std::vector<std::pair<int, int>> possibleList{};
 
+		// Loop and fill the knapsack
+		for (auto it : itemList) {
+			if (it.second <= remainingWeight) {
+				remainingWeight -= it.second;
+				sum += it.first;
+				possibleList.emplace_back(it.first, it.second);
+			}
+		}
 
+		// Sort the container
+		std::sort(possibleList.begin(), possibleList.end());
+
+		// Check if this is the largest profit possible and overwrite if so
+		if (result < sum)
+			result = sum;
+
+		// If this permutation list does not exist in the overall solution set
+		if (solutionSet.find(possibleList) == solutionSet.end()) {
+			// Add to it and print out this permutation
+			solutionSet.insert(possibleList);
+
+			std::cout << "Permutation Set{ " << sum << " : ";
+			for (auto item : possibleList) {
+				std::cout << "[" << item.first << "|" << item.second << "]" << std::endl;
+			}
+			std::cout << "}" << std::endl;
+		}
 
 	} while (std::next_permutation(itemList.begin(), itemList.end(), CompareMemberWeightValue()));
 
 	return result;
 }
 
-int main()
-{
+int main() {
 	std::vector<int>	values	{ 1, 4, 8, 5 },
 						weights{ 3, 3, 5, 6 };
 	int capacity{ 7 };
