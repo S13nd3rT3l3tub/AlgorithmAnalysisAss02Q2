ca75d7ee3f2b0896acff578104c610bed719099d
S13nd3rT3l3tub <40754433+S13nd3rT3l3tub@users.noreply.github.com>
2023-11-10 09:48:05 +0800

    more setup

--- AlgorithmAnalysisAss02Q2.cpp
+++ AlgorithmAnalysisAss02Q2.cpp
@@ -1,28 +1,47 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 #include <map>
 
 // Based on https://www.geeksforgeeks.org/0-1-knapsack-problem-to-print-all-possible-solutions/
 
-int knapSack(std::vector<int> _values,std::vector<int> _weights, int _maxCapacity) {
+struct CompareMemberWeightValue {
+	bool operator()(const std::pair<int, int>& _a, const std::pair<int, int>& _b) const {
+		return _a.second < _b.second;
+	}
+};
+
+int PermutateThroughKnapSack(std::vector<int> _values,std::vector<int> _weights, int _maxCapacity) {
 	// Unequal number of values given; Unable to run algorithm
 	if (_values.size() != _weights.size())
 		return 0;
 
 	// Map to store weights with profits of each item
-	std::map<int, std::pair<int, int>> uMap;
+	//std::map<int, std::pair<int, int>> uMap;
+	std::vector<std::pair<int, int>> itemList{}, possibleList{};
 
-	// Making Pairs and inserting
-	// into the map
+	// Create pair containing value and weight of item and insert into container
+	//for (int i{ 0 }; i < _values.size(); ++i)
+	//	uMap.emplace(i, std::pair<int, int>{_values[i], _weights[i]});
 	for (int i{ 0 }; i < _values.size(); ++i)
-		uMap.emplace(i, std::pair<int, int>{_values[i], _weights[i]});
+		itemList.emplace_back(std::pair<int, int>{_values[i], _weights[i]});
 
+	// Variables for permutation looping
 	//std::set<std::vector<std::pair<int, int>>> set_sol;
 	int result = INT_MIN;
 	int remaining_weight;
 	int sum = 0;
 
-	
+	do {
+		// Set initial values
+		sum = 0;
+		remaining_weight = _maxCapacity;
+		possibleList.clear();
+
+
+
+	} while (std::next_permutation(itemList.begin(), itemList.end(), CompareMemberWeightValue()));
+
 	return result;
 }
 
@@ -32,7 +51,7 @@ int main()
 						weights{ 3, 3, 5, 6 };
 	int capacity{ 7 };
 	
-	int maximum = knapSack(values, weights, capacity);
+	int maximum = PermutateThroughKnapSack(values, weights, capacity);
 	std::cout << "Maximum Profit = ";
 	std::cout << maximum;
 	
